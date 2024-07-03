62cc2f08e4166f67abe67c89e2621529d5be51df
divanster <divanster@gmail.com>
2024-07-03 11:22:40 +0300

    navbar adjustments with bootstrap

--- WebFrontend/src/components/Navbar.tsx
+++ WebFrontend/src/components/Navbar.tsx
@@ -10,7 +10,10 @@ const Navbar: React.FC = () => {
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container-fluid">
         <Link className="navbar-brand" to="/">RecipeApp</Link>
-        <div className="collapse navbar-collapse">
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
               <Link className="nav-link" to="/">Home</Link>
@@ -21,7 +24,7 @@ const Navbar: React.FC = () => {
               </li>
             )}
           </ul>
-          <ul className="navbar-nav ml-auto">
+          <ul className="navbar-nav ms-auto">
             {isAuthenticated ? (
               <li className="nav-item">
                 <button className="btn btn-link nav-link" onClick={logout}>Logout</button>
